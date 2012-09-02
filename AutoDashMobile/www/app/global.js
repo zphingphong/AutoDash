8dd3d1e83da806ced7c3d2c96423e103cb546774
Pan K <pkhantidhara@zinzone.net>
2012-09-01 21:32:30 -0700

    Restart AGAIN!

--- AutoDashMobile/www/app/global.js
+++
@@ -1,8 +0,0 @@
-// global final variables
-var DB;
-var DB_NAME = 'AutoDash'; 
-var DB_VERSION = '1.0';  
-var DB_DISPLAY_NAME = 'Auto Dash'; 
-var DB_SIZE = 65535; 
-
-var DATE_FORMAT = 'Y-m-d H:i';
